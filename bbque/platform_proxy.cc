0898d5d6c61c0c4e294afc1b37ee51b518e3d3ba
Michele Zanella <michele.zanella@polimi.it>
2018-11-23 14:29:54 +0100

    [PP] Moved implementation of GetPlatformDescription inside from .h to .cc
    Signed-off-by: Michele Zanella <michele.zanella@polimi.it>

--- bbque/platform_proxy.cc
+++ bbque/platform_proxy.cc
@@ -3,11 +3,40 @@
 namespace bbque
 {
 
+
 plugins::PlatformLoaderIF * PlatformProxy::pli = nullptr;
 
 bool PlatformProxy::IsHighPerformance(bbque::res::ResourcePathPtr_t const & path) const {
 	(void) path;
 	return false;
+
 }
+
+#ifndef CONFIG_BBQUE_PIL_LEGACY
+	
+const pp::PlatformDescription & PlatformProxy::GetPlatformDescription() {
+
+	std::unique_ptr<bu::Logger> logger = bu::Logger::GetLogger(PLATFORM_PROXY_NAMESPACE);
+
+    // Check if the plugin is never loaded. In that case load it and parse the
+    // platform configuration
+	if (pli == NULL) {
+		logger->Debug("I'm creating a new instance of PlatformLoader plugin.");
+		pli = ModulesFactory::GetModule<plugins::PlatformLoaderIF>(
+			std::string("bq.pl.") + BBQUE_PLOADER_DEFAULT);
+		assert(pli);
+		if (plugins::PlatformLoaderIF::PL_SUCCESS != pli->loadPlatformInfo()) {
+			logger->Fatal("Unable to load platform information.");
+		throw std::runtime_error("PlatformLoaderPlugin pli->loadPlatformInfo() failed.");
+		} else {
+			logger->Info("Platform information loaded successfully.");
+		}
+	}
+
+    // Return the just or previous loaded configuration
+    return pli->getPlatformInfo();
+}
+
+#endif
 
 } //namespace bbque
--- include/bbque/platform_proxy.h
+++ include/bbque/platform_proxy.h
@@ -137,31 +137,10 @@ public:
 
 #ifndef CONFIG_BBQUE_PIL_LEGACY
 	/**
-	* @brief Return the platform description loaded by the relative pugin.
+	* @brief Return the platform description loaded by the relative plugin.
 	*        This method is not available with legacy parser.
 	*/
-	static const pp::PlatformDescription & GetPlatformDescription() {
-
-		std::unique_ptr<bu::Logger> logger = bu::Logger::GetLogger(PLATFORM_PROXY_NAMESPACE);
-
-        // Check if the plugin is never loaded. In that case load it and parse the
-        // platform configuration
-		if (pli == NULL) {
-			logger->Debug("I'm creating a new instance of PlatformLoader plugin.");
-			pli = ModulesFactory::GetModule<plugins::PlatformLoaderIF>(
-				std::string("bq.pl.") + BBQUE_PLOADER_DEFAULT);
-			assert(pli);
-			if (plugins::PlatformLoaderIF::PL_SUCCESS != pli->loadPlatformInfo()) {
-				logger->Fatal("Unable to load platform information.");
-			throw std::runtime_error("PlatformLoaderPlugin pli->loadPlatformInfo() failed.");
-			} else {
-				logger->Info("Platform information loaded successfully.");
-			}
-		}
-
-        // Return the just or previous loaded configuration
-        return pli->getPlatformInfo();
-    }
+	static const pp::PlatformDescription & GetPlatformDescription();
 #endif
 
 protected:
