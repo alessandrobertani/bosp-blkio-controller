2c826c8d385bdcbca5a7dd2922227bf125cb3e95
Giuseppe Massari <joe.massanga@gmail.com>
2020-03-23 12:35:17 +0100

    [Config] bbque-layapp: uniform hierarchy in target names
    Signed-off-by: Giuseppe Massari <joe.massanga@gmail.com>

--- config/templates/cpp/bosp.mk
+++ config/templates/cpp/bosp.mk
@@ -1,48 +1,48 @@
 
-ifdef CONFIG_SAMPLES_MYAPP
+ifdef CONFIG_SAMPLES_APPTYPE_MYAPP
 
 # Targets provided by this project
-.PHONY: myapp clean_myapp
+.PHONY: samples_apptype_myapp clean_samples_apptype_myapp
 
-# Add this to the "contrib_testing" target
-testing: myapp
-clean_testing: clean_myapp
+# Add this to the "samples" targets
+samples_apptype: samples_apptype_myapp
+clean_samples_apptype: clean_samples_apptype_myapp
 
-MODULE_SAMPLES_MYAPP=samples/apptype/MyApp
+MODULE_SAMPLES_APPTYPE_MYAPP=samples/apptype/MyApp
 
-myapp: external
+samples_apptype_myapp: external
 	@echo
 	@echo "==== Building MyApp ($(BUILD_TYPE)) ===="
 	@echo " Using GCC    : $(CC)"
 	@echo " Target flags : $(TARGET_FLAGS)"
 	@echo " Sysroot      : $(PLATFORM_SYSROOT)"
 	@echo " BOSP Options : $(CMAKE_COMMON_OPTIONS)"
-	@[ -d $(MODULE_SAMPLES_MYAPP)/build/$(BUILD_TYPE) ] || \
-		mkdir -p $(MODULE_SAMPLES_MYAPP)/build/$(BUILD_TYPE) || \
+	@[ -d $(MODULE_SAMPLES_APPTYPE_MYAPP)/build/$(BUILD_TYPE) ] || \
+		mkdir -p $(MODULE_SAMPLES_APPTYPE_MYAPP)/build/$(BUILD_TYPE) || \
 		exit 1
-	@cd $(MODULE_SAMPLES_MYAPP)/build/$(BUILD_TYPE) && \
+	@cd $(MODULE_SAMPLES_APPTYPE_MYAPP)/build/$(BUILD_TYPE) && \
 		CC=$(CC) CFLAGS="$(TARGET_FLAGS)" \
 		CXX=$(CXX) CXXFLAGS="$(TARGET_FLAGS)" \
 		cmake $(CMAKE_COMMON_OPTIONS) ../.. || \
 		exit 1
-	@cd $(MODULE_SAMPLES_MYAPP)/build/$(BUILD_TYPE) && \
+	@cd $(MODULE_SAMPLES_APPTYPE_MYAPP)/build/$(BUILD_TYPE) && \
 		make -j$(CPUS) install || \
 		exit 1
 
-clean_myapp:
+clean_samples_apptype_myapp:
 	@echo
 	@echo "==== Clean-up MyApp Application ===="
 	@[ ! -f $(BUILD_DIR)/usr/bin/myapp ] || \
 		rm -f $(BUILD_DIR)/etc/bbque/recipes/MyApp*; \
 		rm -f $(BUILD_DIR)/usr/bin/myapp*
-	@rm -rf $(MODULE_SAMPLES_MYAPP)/build
+	@rm -rf $(MODULE_SAMPLES_APPTYPE_MYAPP)/build
 	@echo
 
-else # CONFIG_SAMPLES_MYAPP
+else # CONFIG_SAMPLES_APPTYPE_MYAPP
 
-myapp:
-	$(warning contib module MyApp disabled by BOSP configuration)
+samples_apptype_myapp:
+	$(warning MyApp module disabled by BOSP configuration)
 	$(error BOSP compilation failed)
 
-endif # CONFIG_SAMPLES_MYAPP
+endif # CONFIG_SAMPLES_APPTYPE_MYAPP
 
