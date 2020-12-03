71f5b53af5cdaa87b120c02e1e769d7c0f1d63f5
Giuseppe Massari <joe.massanga@gmail.com>
2020-12-03 16:31:49 +0100

    [RCP] Minor changes
    Signed-off-by: Giuseppe Massari <joe.massanga@gmail.com>

--- bbque/app/recipe.cc
+++ bbque/app/recipe.cc
@@ -26,12 +26,12 @@
 namespace ba = bbque::app;
 namespace br = bbque::res;
 
-namespace bbque { namespace app {
+namespace bbque {
+namespace app {
 
-
-Recipe::Recipe(std::string const & name):
-		pathname(name) {
-
+Recipe::Recipe(std::string const & name) :
+    pathname(name)
+{
 	// Get a logger
 	std::string logger_name(RECIPE_NAMESPACE"." + name);
 	logger = bu::Logger::GetLogger(logger_name.c_str());
@@ -50,24 +50,25 @@ Recipe::Recipe(std::string const & name):
 	working_modes.resize(MAX_NUM_AWM);
 }
 
-Recipe::~Recipe() {
+Recipe::~Recipe()
+{
 	working_modes.clear();
 	constraints.clear();
 }
 
-AwmPtr_t const Recipe::AddWorkingMode(
-		uint8_t _id,
-		std::string const & _name,
-		uint8_t _value) {
+AwmPtr_t const Recipe::AddWorkingMode(uint8_t _id,
+				      std::string const & _name,
+				      uint8_t _value)
+{
 	// Check if the AWMs are sequentially numbered
 	if (_id != last_awm_id) {
 		logger->Error("AddWorkingModes: Found ID = %d. Expected %d",
-				_id, last_awm_id);
+			_id, last_awm_id);
 		return AwmPtr_t();
 	}
 
 	// Insert a new working mode descriptor into the vector
-	AwmPtr_t new_awm(new app::WorkingMode(_id, _name, _value));
+	auto new_awm = std::make_shared<app::WorkingMode>(_id, _name, _value);
 	if (new_awm == nullptr) {
 		logger->Error("AddWorkingMode: Error in new AWM	creation");
 		return AwmPtr_t();
@@ -80,11 +81,11 @@ AwmPtr_t const Recipe::AddWorkingMode(
 	return working_modes[_id];
 }
 
-void Recipe::AddConstraint(
-		std::string const & rsrc_path,
-		uint64_t lb,
-		uint64_t ub) {
-	// Check resource existance
+void Recipe::AddConstraint(std::string const & rsrc_path,
+			   uint64_t lb,
+			   uint64_t ub)
+{
+	// Check resource existence
 	ResourceAccounter & ra(ResourceAccounter::GetInstance());
 	ResourcePathPtr_t const r_path(ra.GetPath(rsrc_path));
 	if (!r_path)
@@ -97,22 +98,23 @@ void Recipe::AddConstraint(
 		(cons_it->second)->lower = std::max((cons_it->second)->lower, lb);
 		(cons_it->second)->upper = std::max((cons_it->second)->upper, ub);
 		logger->Debug("Constraint (edit): %s L=%d U=%d",
-				r_path->ToString().c_str(),
-				(cons_it->second)->lower,
-				(cons_it->second)->upper);
+			r_path->ToString().c_str(),
+			(cons_it->second)->lower,
+			(cons_it->second)->upper);
 		return;
 	}
 
 	// Insert a new constraint
 	constraints.insert(std::pair<ResourcePathPtr_t, ConstrPtr_t>(
-				r_path, ConstrPtr_t(new br::ResourceConstraint(lb, ub))));
+			r_path, ConstrPtr_t(new br::ResourceConstraint(lb, ub))));
 	logger->Debug("Constraint (new): %s L=%" PRIu64 " U=%" PRIu64,
-					r_path->ToString().c_str(),
-					constraints[r_path]->lower,
-					constraints[r_path]->upper);
+		r_path->ToString().c_str(),
+		constraints[r_path]->lower,
+		constraints[r_path]->upper);
 }
 
-void Recipe::Validate() {
+void Recipe::Validate()
+{
 	working_modes.resize(last_awm_id);
 
 	// Validate each AWM according to current resources total availability
@@ -127,7 +129,8 @@ void Recipe::Validate() {
 	Normalize();
 }
 
-void Recipe::UpdateNormalInfo(AwmNormalInfo & info, uint32_t last_value) {
+void Recipe::UpdateNormalInfo(AwmNormalInfo & info, uint32_t last_value)
+{
 	// Update the max value
 	if (last_value > info.max)
 		info.max = last_value;
@@ -142,13 +145,14 @@ void Recipe::UpdateNormalInfo(AwmNormalInfo & info, uint32_t last_value) {
 	// Debug logging
 	if (info.attr == VALUE)
 		logger->Debug("AWMs values max: %d, min: %d, delta: %d",
-				info.max, info.min, info.delta);
+			info.max, info.min, info.delta);
 	else if (info.attr == CONFIG_TIME)
 		logger->Debug("AWMs configuration times max: %d, min: %d, delta: %d",
-				info.max, info.min, info.delta);
+			info.max, info.min, info.delta);
 }
 
-void Recipe::Normalize() {
+void Recipe::Normalize()
+{
 	// Set of AWMs: normalize attributes
 	for (int i = 0; i < last_awm_id; ++i) {
 		if (working_modes[i]->Disabled()) continue;
@@ -157,41 +161,43 @@ void Recipe::Normalize() {
 	}
 }
 
-void Recipe::NormalizeValue(uint8_t awm_id) {
+void Recipe::NormalizeValue(uint8_t awm_id)
+{
 	float normal_value = 0.0;
 
 	// Normalize the value
 	if (values.delta > 0)
 		normal_value =
-			static_cast<float>(working_modes[awm_id]->RecipeValue()) / values.max;
-	// There is only one AWM in the recipe
+		static_cast<float>(working_modes[awm_id]->RecipeValue()) / values.max;
+		// There is only one AWM in the recipe
 	else if (working_modes.size() == 1)
 		normal_value = 1.0;
-	// Penalize set of working modes having always the same QoS value
+		// Penalize set of working modes having always the same QoS value
 	else
 		normal_value = 0.0;
 
 	// Set the normalized value into the AWM
 	working_modes[awm_id]->SetNormalValue(normal_value);
 	logger->Info("AWM %d normalized value = %.2f ",
-					working_modes[awm_id]->Id(),
-					working_modes[awm_id]->Value());
+		working_modes[awm_id]->Id(),
+		working_modes[awm_id]->Value());
 }
 
-void Recipe::NormalizeConfigTime(uint8_t awm_id) {
+void Recipe::NormalizeConfigTime(uint8_t awm_id)
+{
 	float normal_time = 0.0;
 	if (config_times.delta > 0)
 		normal_time =
-			static_cast<float>
-					(working_modes[awm_id]->RecipeConfigTime() - config_times.min) /
-				(config_times.delta);
+		static_cast<float>
+		(working_modes[awm_id]->RecipeConfigTime() - config_times.min) /
+		(config_times.delta);
 	else
 		normal_time = 0.0;
 
 	working_modes[awm_id]->SetNormalConfigTime(normal_time);
 	logger->Info("AWM %d normalized config time = %.2f ",
-					working_modes[awm_id]->Id(),
-					working_modes[awm_id]->ConfigTime());
+		working_modes[awm_id]->Id(),
+		working_modes[awm_id]->ConfigTime());
 }
 
 } // namespace app
