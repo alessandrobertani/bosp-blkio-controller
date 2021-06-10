8e2a855cc499ad7fa6e9b11a61022ae6a7484d74
Alessandro <alessandro.bertani@mail.polimi.it>
2021-06-10 21:53:05 +0200

    [PP_Linux] Added attributes for read and write bandwidth.
    Signed-off-by: Alessandro <alessandro.bertani@mail.polimi.it>

--- include/bbque/pp/linux_platform_proxy_types.h
+++ include/bbque/pp/linux_platform_proxy_types.h
@@ -66,6 +66,8 @@ struct RLinuxBindings_t
 	uint_fast32_t amount_cpus = 0; /** Percentage of CPUs time assigned */
 	int_fast64_t amount_memb = 0; /** Amount of socket MEMORY assigned (byte) */
 	int_fast64_t amount_net_bw = 0; /** Amount of network bandwidth assigned (bps) */
+	int_fast64_t amount_read_bw = 0; /** Amount of read bandwidth assigned (MB/s)*/
+	int_fast64_t amount_write_bw = 0; /* Amount of write bandwidth assigned (MB/s)*/
 
 	RLinuxBindings_t(const uint_fast8_t MaxCpusCount,
 			const uint_fast8_t MaxMemsCount)
